45106654a5862550fd755c31babcc0cb25b4f205
esimm98 <esimm98@gmail.com>
2015-11-22 21:33:16 -0700

    Initial drawing set up

--- planet.rb
+++ planet.rb
@@ -1,9 +1,21 @@
+require 'gosu'
+
 class Planet
 
-	attr_reader :xpos, :ypos, :xvel, :yvel, :mass, :file
+	G = 6.67408*10**-11
 
-	def initialize(xpos, ypos, xvel, y)
+	def initialize(xpos, ypos, xvel, yvel, mass, file)
+		@xpos = xpos
+		@ypos = ypos
+		@xvel = xvel
+		@yvel = yvel
+		@mass = mass
+		@image = Gosu::Image.new("images/#{file}")
+		@net_force = 0
+	end
 
+	def draw
+		@image.draw(@xpos, @ypos, ZOrder::Planets)
 	end
 
 end
--- run.rb
+++ run.rb
@@ -1,34 +1,42 @@
 require_relative "zorder"
+require_relative "planet"
 require 'gosu'
 
-class GameWindow < Gosu::Window
+class SimWindow < Gosu::Window
 
 	def initialize
-		super 1000, 1000
-		self.caption = "Planet Orbits Thingy"
+		@width = 1000
+		@height = 600
+		super @width, @height
+		self.caption = "Planet Orbit Sim"
 		@background = Gosu::Image.new("images/space.jpg")
-		G = 6.67408*10**-11
-		planets = []
+		@planets = []
+		@line = 0
+		@system_size = 0
 
 		file = File.read("Planets.txt")
-		file.each_line do |i|
-
+		file.each_line do |line|
+			arr = line.split(' ')
+			if @line == 1
+				@system_size = arr[0].to_f*2
+			elsif @line >= 2
+				arr[0] = arr[0].to_f/@system_size*@width + @width/2
+				arr[1] = arr[1].to_f/@system_size*@height + @height/2
+				@planets.push << Planet.new(arr[0], arr[1], arr[2].to_f, arr[3].to_f, arr[4].to_f, arr[5])
+			end
+			@line += 1
 		end
 	end
 
 	def draw
 		@background.draw(0, 0, ZOrder::Background)
-	end
-
-	def make 
-
+		@planets.each { |planet| planet.draw }
 	end
 
 	def button_down(id)
 		close if id == Gosu::KbEscape
 	end
-
 end
 
-window = GameWindow.new
+window = SimWindow.new
 window.show
